e6068431fd5f97f7830f6ddf58e513417be5b4a0
Le0v1n <67271439+Le0v1n@users.noreply.github.com>
2022-12-25 18:59:18 +0800

    FCN

---
+++ Segmentation/FCN/transforms.py
@@ -0,0 +1,117 @@
+import numpy as np
+import random
+
+import torch
+from torchvision import transforms as T
+from torchvision.transforms import functional as F
+
+
+def pad_if_smaller(img, size, fill=0):
+    """
+    如果图像最小边长小于给定size，则用数值fill进行padding
+    Args:
+        img: 传入图片大小
+        size: Crop大小
+        fill: 填充时使用的数值
+
+    Returns: (填充)后的图片
+
+    """
+    min_size = min(img.size)
+    if min_size < size:
+        ow, oh = img.size
+        padh = size - oh if oh < size else 0
+        padw = size - ow if ow < size else 0
+        img = F.pad(img, (0, 0, padw, padh), fill=fill)
+    return img
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+
+class RandomResize(object):
+    def __init__(self, min_size, max_size=None):
+        self.min_size = min_size
+        if max_size is None:
+            max_size = min_size
+        self.max_size = max_size
+
+    def __call__(self, image, target):
+        """
+        Args:
+            image: 传入的图像
+            target: 对应的标签图片
+
+        Returns: 最小边缩放后的图片及其对应的标签图像
+
+        """
+        size = random.randint(self.min_size, self.max_size)
+        # 这里size传入的是int类型，所以是将图像的最小边长缩放到size大小
+        image = F.resize(image, size)
+        # 这里的interpolation注意下，在torchvision(0.9.0)以后才有InterpolationMode.NEAREST
+        # 如果是之前的版本需要使用PIL.Image.NEAREST
+        target = F.resize(target, size, interpolation=T.InterpolationMode.NEAREST)
+        return image, target
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, flip_prob):
+        self.flip_prob = flip_prob
+
+    def __call__(self, image, target):
+        if random.random() < self.flip_prob:
+            image = F.hflip(image)
+            target = F.hflip(target)
+        return image, target
+
+
+class RandomCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, target):
+        # 首先判断图片和Crop的大小关系：如果图片的某一个边小于Crop大小，那么需要对图片进行padding
+        image = pad_if_smaller(image, self.size)
+
+        # 如果原图进行了padding，那么标签图片也需要进行填充，因为255表示背景，所以标签文图片使用255作为填充值
+        target = pad_if_smaller(target, self.size, fill=255)
+
+        # 对图片进行裁剪
+        crop_params = T.RandomCrop.get_params(image, (self.size, self.size))
+        image = F.crop(image, *crop_params)
+        target = F.crop(target, *crop_params)
+        return image, target
+
+
+class CenterCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, target):
+        image = F.center_crop(image, self.size)
+        target = F.center_crop(target, self.size)
+        return image, target
+
+
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        target = torch.as_tensor(np.array(target), dtype=torch.int64)
+        return image, target
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, image, target):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        return image, target
