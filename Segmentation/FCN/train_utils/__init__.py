356453fcd78398ccd14c5e48f8fe88986d6890c2
Le0v1n <67271439+Le0v1n@users.noreply.github.com>
2023-07-31 14:27:01 +0800

    Add files via upload

--- Segmentation/FCN/train_utils/__init__.py
+++ Segmentation/FCN/train_utils/__init__.py
@@ -1,2 +1,2 @@
-from .train_and_eval import train_one_epoch, evaluate, create_lr_scheduler
+from .train_and_eval import train_one_epoch, train_one_epoch_for_mobilenet, evaluate, evaluate_for_mobilenetv2, create_lr_scheduler
 from .distributed_utils import init_distributed_mode, save_on_master, mkdir
